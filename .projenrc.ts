ae57d3c7f7e22ed6740193e52ff380c2dee383b7
Braden Mars <bradenmars@bradenmars.me>
2023-04-12 00:59:56 -0500

    feat(vue): define vue.ui.text package.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,6 +1,7 @@
-import { javascript } from "projen";
 import { nx_monorepo } from "aws-prototyping-sdk";
-const project = new nx_monorepo.NxMonorepoProject({
+import { javascript, typescript } from "projen";
+
+const monorepo = new nx_monorepo.NxMonorepoProject({
   authorEmail: "support@arroyodev.com",
   authorName: "arroyoDev",
   authorOrganization: true,
@@ -9,7 +10,7 @@ const project = new nx_monorepo.NxMonorepoProject({
   devContainer: true,
   devDeps: ["aws-prototyping-sdk"],
   docgen: true,
-  name: "nx-monorepo",
+  name: "components",
   packageManager: javascript.NodePackageManager.PNPM,
   pnpmVersion: "8",
   prettier: true,
@@ -20,4 +21,16 @@ const project = new nx_monorepo.NxMonorepoProject({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
-project.synth();
+
+new typescript.TypeScriptProject({
+  parent: monorepo,
+  outdir: "packages/vue/ui/text",
+  name: "vue.ui.text",
+  defaultReleaseBranch: "main",
+  packageName: "@arroyodev-llc/components.vue.ui.text",
+  packageManager: javascript.NodePackageManager.PNPM,
+  pnpmVersion: "8",
+  deps: ["vue"]
+});
+
+monorepo.synth();

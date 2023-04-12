e87403ad7609d915f9b9e754950516338a20601f
Braden Mars <bradenmars@bradenmars.me>
2023-04-12 01:57:20 -0500

    feat(projen): add VueComponentProject.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,6 +1,10 @@
 import { nx_monorepo } from "aws-prototyping-sdk";
 import { javascript, typescript } from "projen";
-import { TypeScriptModuleResolution } from "projen/lib/javascript";
+import {
+  type TypescriptConfigOptions,
+  TypeScriptModuleResolution,
+} from "projen/lib/javascript";
+import { TypeScriptProjectOptions } from "projen/lib/typescript";
 
 const monorepo = new nx_monorepo.NxMonorepoProject({
   authorEmail: "support@arroyodev.com",
@@ -17,31 +21,54 @@ const monorepo = new nx_monorepo.NxMonorepoProject({
   prettier: true,
   projenrcTs: true,
   renovatebot: true,
+  gitignore: [".idea/**"],
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
 
-new typescript.TypeScriptProject({
-  parent: monorepo,
-  outdir: "packages/vue/ui/text",
-  name: "vue.ui.text",
-  defaultReleaseBranch: "main",
-  packageName: "@arroyodev-llc/components.vue.ui.text",
-  packageManager: javascript.NodePackageManager.PNPM,
-  pnpmVersion: "8",
-  deps: ["vue"],
-  jest: false,
-  tsconfig: {
-    compilerOptions: {
-      skipLibCheck: true,
-      moduleResolution: TypeScriptModuleResolution.NODE,
-      module: "ESNext",
-      target: "ESNext",
-      lib: ["ESNext", "DOM"],
-    },
+const vueTsConfig: TypescriptConfigOptions = {
+  compilerOptions: {
+    skipLibCheck: true,
+    moduleResolution: TypeScriptModuleResolution.NODE,
+    module: "ESNext",
+    target: "ESNext",
+    lib: ["ESNext", "DOM"],
   },
+};
+
+interface VueComponentProjectOptions
+  extends Omit<TypeScriptProjectOptions, "defaultReleaseBranch"> {
+  defaultReleaseBranch?: string;
+}
+
+class VueComponentProject extends typescript.TypeScriptProject {
+  constructor(options: VueComponentProjectOptions) {
+    const { name } = options;
+    const namePath = name.split(".").join("/");
+    const defaultOutDir = `packages/${namePath}`;
+    const defaultPackageName = `@arroyodev-llc/components.${name}`;
+    super({
+      defaultReleaseBranch: "main",
+      tsconfig: vueTsConfig,
+      packageManager: javascript.NodePackageManager.PNPM,
+      pnpmVersion: "8",
+      outdir: defaultOutDir,
+      packageName: defaultPackageName,
+      jest: false,
+      ...options,
+    });
+
+    this.addDeps("vue", "@vue/runtime-dom");
+
+    this.addDevDeps("typescript", "vitest");
+  }
+}
+
+new VueComponentProject({
+  parent: monorepo,
+  name: "vue.ui.text",
 });
 
 monorepo.synth();

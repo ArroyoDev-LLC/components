301dc92e953aa06cc89230fe14567fa63007e95a
Braden Mars <bradenmars@bradenmars.me>
2023-04-13 06:03:51 -0500

    feat(vue): use sfc for Text vue component.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/vue/ui/text/src/Text.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import type {TextVariant} from './types.ts'
+
+export interface TextProps {
+  variant?: TextVariant;
+}
+import type { TextProps } from "../lib/Text";
+
+const props = withDefaults(defineProps<TextProps>(), { variant: "body" });
+
+const anchor = computed(() =>
+  props.variant === "body"
+    ? "p"
+    : typeof props.variant === "number"
+      ? `h${props.variant}`
+      : props.variant
+);
+</script>
+
+<template>
+  <component :is="anchor">
+    <slot/>
+  </component>
+</template>
--- packages/vue/ui/text/src/index.ts
+++ packages/vue/ui/text/src/index.ts
@@ -1,1 +1,2 @@
-export * from "./Text";
+export * from './Text.vue';
+export type * from './types.ts';

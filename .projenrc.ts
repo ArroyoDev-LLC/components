018902a34abad1ea17d66b8f6c0c395fe1a78951
Braden Mars <bradenmars@bradenmars.me>
2023-04-14 03:48:56 -0500

    feat(projen): add unbuild component.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,10 +1,18 @@
 import { nx_monorepo } from "aws-prototyping-sdk";
-import { DependencyType, javascript, typescript } from "projen";
+import {
+  Component,
+  DependencyType,
+  javascript,
+  Project,
+  TextFile,
+  typescript,
+} from "projen";
 import {
   type TypescriptConfigOptions,
   TypeScriptModuleResolution,
 } from "projen/lib/javascript";
 import { TypeScriptProjectOptions } from "projen/lib/typescript";
+import type { BuildConfig as UnBuildBuildConfig } from "unbuild";
 
 const monorepo = new nx_monorepo.NxMonorepoProject({
   authorEmail: "support@arroyodev.com",
@@ -41,9 +49,10 @@ const monorepo = new nx_monorepo.NxMonorepoProject({
     "vitest",
     "rollup-plugin-vue",
     "tsx",
+    "unbuild",
   ],
 });
-monorepo.gitignore.exclude(".idea", ".idea/**")
+monorepo.gitignore.exclude(".idea", ".idea/**");
 monorepo.defaultTask.reset("tsx .projenrc.ts");
 monorepo.tsconfig.addInclude("**/*.ts");
 monorepo.tsconfigDev.addInclude("**/*.ts");
@@ -71,6 +80,54 @@ interface VueComponentProjectOptions
   defaultReleaseBranch?: string;
 }
 
+interface UnBuildOptions {
+  vue?: boolean;
+  options?: UnBuildBuildConfig;
+}
+
+class UnBuild extends Component {
+  public static of(project: Project): UnBuild | undefined {
+    const isUnBuild = (o: Component): o is UnBuild => o instanceof UnBuild;
+    return project.components.find(isUnBuild);
+  }
+
+  readonly vue: boolean;
+  readonly options: UnBuildBuildConfig;
+  readonly file: TextFile;
+
+  constructor(project: Project, options: UnBuildOptions = {}) {
+    super(project);
+    this.vue = options.vue ?? false;
+    this.options = options.options ?? {};
+
+    this.project.deps.addDependency("unbuild", DependencyType.BUILD);
+    this.project.deps.addDependency("rollup-plugin-vue", DependencyType.BUILD);
+    this.file = new TextFile(this.project, "build.config.ts", {
+      lines: [
+        `import { defineBuildConfig } from 'unbuild'`,
+        ...(this.vue && [`import vue from 'rollup-plugin-vue'`]),
+        `export default defineBuildConfig(${this.buildConfig}});`,
+      ],
+    });
+  }
+
+  get buildConfig(): string {
+    const config = this.config ? `...${JSON.stringify(this.config)}}` : ``;
+    if (this.vue) {
+      return `{
+      ${config}
+    hooks: {
+      'rollup:options': (ctx, options) => {
+        // @ts-expect-error ignore
+        options.plugins.push(vue());
+      },
+    },
+      `;
+    }
+    return `{ ${JSON.stringify(this.options)} }`;
+  }
+}
+
 class VueComponentProject extends typescript.TypeScriptProject {
   constructor(options: VueComponentProjectOptions) {
     const { name } = options;
@@ -88,8 +145,10 @@ class VueComponentProject extends typescript.TypeScriptProject {
       ...options,
     });
 
+    new UnBuild(this, { vue: true });
+
     this.addDeps("vue", "@vue/runtime-dom");
-    this.addDevDeps("typescript", "vitest", "unbuild", "rollup-plugin-vue");
+    this.addDevDeps("typescript", "vitest");
 
     this.deps.addDependency("eslint-plugin-vue", DependencyType.DEVENV);
     this.eslint!.addPlugins("eslint-plugin-vue");
@@ -97,6 +156,7 @@ class VueComponentProject extends typescript.TypeScriptProject {
 
     this.tsconfig!.addInclude("../../../../env.d.ts");
     this.tsconfig!.addInclude("src/**/*.vue");
+    this.tsconfigDev!.addInclude("build.config.ts");
     this.tasks.removeTask("build");
     this.tasks.addTask("build", { exec: "unbuild" });
   }

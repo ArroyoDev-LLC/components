901050997fa7717a686b3b4c8e5813f5ca420352
Braden Mars <bradenmars@bradenmars.me>
2023-04-14 03:49:22 -0500

    chore(projen): update generated files.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/vue/ui/text/build.config.ts
+++ packages/vue/ui/text/build.config.ts
@@ -1,11 +1,12 @@
-import vue from 'rollup-plugin-vue';
-import { defineBuildConfig } from 'unbuild';
+import { defineBuildConfig } from "unbuild";
+import vue from "rollup-plugin-vue";
 
 export default defineBuildConfig({
+
   hooks: {
-    'rollup:options': (ctx, options) => {
+    "rollup:options": (ctx, options) => {
       // @ts-expect-error ignore
       options.plugins.push(vue());
-    },
-  },
+    }
+  }
 });

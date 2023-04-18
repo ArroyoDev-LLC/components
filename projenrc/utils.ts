edb4ea7e37d4de0a7d0fe02d5206b2ca1e0785de
Braden Mars <bradenmars@bradenmars.me>
2023-04-18 01:51:13 -0500

    feat(projerc): add `applyOverrides` utility.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ projenrc/utils.ts
@@ -0,0 +1,18 @@
+interface SupportsOverrides {
+	addOverride(key: string, value: any): void
+}
+
+/**
+ * Apply overrides from an object.
+ * @param item - item to apply too.
+ * @param overrides - record of overrides.
+ */
+export const applyOverrides = <T extends SupportsOverrides>(
+	item: T,
+	overrides: Record<string, any>
+): T => {
+	Object.entries(overrides).forEach(([key, value]) =>
+		item.addOverride(key, value)
+	)
+	return item
+}

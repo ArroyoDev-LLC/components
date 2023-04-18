a3e130ccdb688ac7b57e9f9e3ea75bfa11190a43
Braden Mars <bradenmars@bradenmars.me>
2023-04-18 01:39:05 -0500

    chore(projen): update all managed files.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/vue/ui/button/src/Button.vue
+++ packages/vue/ui/button/src/Button.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import PrimeButton from "primevue/button";
+import PrimeButton from 'primevue/button';
 
 export interface ButtonProps {
-  size: "sm" | "md" | "lg";
-  variant: "primary" | "outline" | "text";
+  size: 'sm' | 'md' | 'lg';
+  variant: 'primary' | 'outline' | 'text';
 }
 
 const props = withDefaults(defineProps<ButtonProps>(), {
-  size: "md",
-  variant: "primary",
+  size: 'md',
+  variant: 'primary',
 });
 </script>
 

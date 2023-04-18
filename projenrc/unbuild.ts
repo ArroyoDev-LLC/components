bac8b12b497707d5ee35daef5ff6d576cedf3d8f
Braden Mars <bradenmars@bradenmars.me>
2023-04-18 04:04:37 -0500

    feat(projen): use `TypeScriptSourceFile` for building unbuild config.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- projenrc/unbuild.ts
+++ projenrc/unbuild.ts
@@ -1,14 +1,17 @@
-import { Component, DependencyType, Project, SourceCode } from 'projen'
-import { BuildConfig as UnBuildBuildConfig } from 'unbuild'
+import { Component, DependencyType } from 'projen'
 import { ModuleImports } from 'projen/lib/javascript/render-options'
+import { TypeScriptProject } from 'projen/lib/typescript'
+import { SyntaxKind } from 'ts-morph'
+import { BuildConfig as UnBuildBuildConfig } from 'unbuild'
+import { TypeScriptSourceFile } from './typescript-source-file.ts'
 
-interface UnBuildOptions {
+export interface UnBuildOptions {
 	vue?: boolean
 	options?: UnBuildBuildConfig
 }
 
 export class UnBuild extends Component {
-	public static of(project: Project): UnBuild | undefined {
+	public static of(project: TypeScriptProject): UnBuild | undefined {
 		const isUnBuild = (o: Component): o is UnBuild => o instanceof UnBuild
 		return project.components.find(isUnBuild)
 	}
@@ -16,48 +19,75 @@ export class UnBuild extends Component {
 	readonly vue: boolean
 	readonly options: UnBuildBuildConfig
 
-	file: SourceCode
-	readonly imports: ModuleImports
+	file: TypeScriptSourceFile
 
-	constructor(project: Project, options: UnBuildOptions = {}) {
+	constructor(
+		public readonly project: TypeScriptProject,
+		options: UnBuildOptions = {}
+	) {
 		super(project)
 		this.vue = options.vue ?? false
 		this.options = options.options ?? {}
-
-		this.file = new SourceCode(this.project, 'build.config.ts', {
-			readonly: true,
-		})
-		this.file.line(`// ${this.file.marker}`)
-
 		this.project.deps.addDependency('unbuild', DependencyType.BUILD)
-		this.imports = new ModuleImports()
-		this.imports.add('unbuild', 'defineBuildConfig')
+		this.project.tsconfigDev.addInclude('build.config.ts')
+		this.project.eslint!.addOverride({
+			files: ['build.config.ts'],
+			rules: {
+				'import/no-extraneous-dependencies': ['off'],
+			},
+		})
 
+		const unbuildImports = new ModuleImports()
+		unbuildImports.add('unbuild', 'defineBuildConfig')
+		unbuildImports.add('unbuild', 'type BuildConfig')
+		const imports = []
 		if (this.vue) {
+			unbuildImports.add('unbuild', 'type BuildContext')
+			unbuildImports.add('rollup', 'type RollupOptions')
+			imports.push(`import vue from 'rollup-plugin-vue'`)
+			this.project.deps.addDependency('rollup', DependencyType.BUILD)
 			this.project.deps.addDependency('rollup-plugin-vue', DependencyType.BUILD)
-			this.file.line(`import vue from "rollup-plugin-vue";`)
 		}
+		imports.unshift(unbuildImports.asEsmImports().join('\n'))
 
-		this.imports.asEsmImports().map((imp) => this.file.line(imp))
-		this.file.open(`export default defineBuildConfig({`)
+		const source = `${imports.join('\n')}
 
-		const optionsSource = Object.keys(this.options).length
-			? '...' + JSON.stringify(this.options) + ','
-			: ''
-		const hooksSource = this.options?.hooks
-			? '...' + JSON.stringify(this.options.hooks) + ','
-			: ''
+const config: BuildConfig = {}
+export default defineBuildConfig(config)`
 
-		this.file.line(optionsSource)
-		this.file.open('hooks: {')
-		if (this.vue) {
-			this.file.open(`'rollup:options': (ctx, options) => {`)
-			this.file.line('// @ts-expect-error ignore rollup')
-			this.file.line('options.plugins.push(vue());')
-			this.file.close('},')
-		}
-		this.file.line(hooksSource)
-		this.file.close('}')
-		this.file.close('})')
+		this.file = new TypeScriptSourceFile(project, 'build.config.ts', {
+			source,
+			transformer: (sourceFile) => {
+				const cfgNode = sourceFile.getVariableDeclarationOrThrow('config')
+				const configExpr = cfgNode.getInitializerIfKindOrThrow(
+					SyntaxKind.ObjectLiteralExpression
+				)
+				configExpr.addPropertyAssignments(
+					Object.entries(this.options).map(([key, value]) => ({
+						name: key,
+						initializer:
+							typeof value === 'string' ? `'${value}'` : value.toString(),
+					}))
+				)
+				if (this.vue) {
+					const hooksExpr = configExpr.addPropertyAssignment({
+						name: 'hooks',
+						initializer: '{}',
+					})
+					const hooksExprInit = hooksExpr.getInitializerIfKindOrThrow(
+						SyntaxKind.ObjectLiteralExpression
+					)
+					hooksExprInit.addPropertyAssignment({
+						name: `'rollup:options'`,
+						initializer: `(ctx: BuildContext, options: RollupOptions) => {
+							// @ts-expect-error ignore rollup.
+							options.plugins.push(vue())
+						}`,
+					})
+				}
+			},
+		})
+
 	}
+
 }

92459af10dd3932607086ca1b055ad2ea658fca7
Braden Mars <bradenmars@bradenmars.me>
2023-04-28 02:12:37 -0500

    feat(projenrc): `NxMonorepoProjectOptionsBuilder`, workspaceDeps, tsconfigBase types, mixin
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- projenrc/option-builders.ts
+++ projenrc/option-builders.ts
@@ -1,7 +1,60 @@
+import type nx_monorepo from '@aws-prototyping-sdk/nx-monorepo'
+import { CollectionKind, PrimitiveType } from '@jsii/spec'
 import { ProjenStruct, Struct } from '@mrgrain/jsii-struct-builder'
 import path from 'pathe'
 import { type cdk, Component, type typescript } from 'projen'
 import LintConfig from './lint-config.ts'
+
+const typescriptStructMixin = Struct.empty()
+	.add({
+		name: 'workspaceDeps',
+		optional: true,
+		docs: {
+			summary: 'Dependencies linked by the `workspace` protocol.',
+		},
+		type: {
+			collection: {
+				kind: CollectionKind.Array,
+				elementtype: {
+					union: {
+						types: [
+							{ fqn: 'projen.javascript.NodeProject' },
+							{ primitive: PrimitiveType.String },
+						],
+					},
+				},
+			},
+		},
+	})
+	.add({
+		name: 'tsconfigBase',
+		optional: true,
+		docs: {
+			summary: 'TypescriptConfigExtends to use as base.',
+		},
+		type: { fqn: 'projen.javascript.TypescriptConfigExtends' },
+	})
+
+export class NxMonorepoProjectOptionsBuilder extends Component {
+	constructor(project: nx_monorepo.NxMonorepoProject) {
+		super(project)
+
+		const filePath = path.join('projenrc', 'nx-monorepo-project-options.ts')
+		const struct = new ProjenStruct(project, {
+			name: 'NxMonorepoProjectOptions',
+			filePath,
+		})
+
+		struct.mixin(
+			Struct.fromFqn(
+				'@aws-prototyping-sdk/nx-monorepo.NxMonorepoProjectOptions'
+			)
+				.withoutDeprecated()
+				.omit('defaultReleaseBranch'),
+			typescriptStructMixin
+		)
+	}
+}
 
 export class ProjenProjectOptionsBuilder extends Component {
 	constructor(project: typescript.TypeScriptProject) {
@@ -47,7 +100,8 @@ export class TypeScriptProjectOptionsBuilder extends Component {
 		struct.mixin(
 			Struct.fromFqn('projen.typescript.TypeScriptProjectOptions')
 				.withoutDeprecated()
-				.omit('defaultReleaseBranch')
+				.omit('defaultReleaseBranch'),
+			typescriptStructMixin
 		)
 
 		LintConfig.of(project)?.eslint?.addIgnorePattern?.(filePath)

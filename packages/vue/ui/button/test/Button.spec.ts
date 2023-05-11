af65df7ce7c9ea6d0d12f7ac284a59f7aaf90c40
Braden Mars <bradenmars@bradenmars.me>
2023-05-11 00:32:12 -0500

    test(vue.ui.button): utilize shallow mount for primevue button
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/vue/ui/button/test/Button.spec.ts
+++ packages/vue/ui/button/test/Button.spec.ts
@@ -1,10 +1,10 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
 import Button from '../src/Button.vue'
 
 describe('Button', () => {
 	it('renders as expected.', async () => {
-		const mnt = mount(Button)
+		const mnt = shallowMount(Button)
 		expect(mnt.element).toMatchSnapshot()
 	})
 })

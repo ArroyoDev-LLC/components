c4dfa6f12c6025949af263fa01597bc9de75fa52
Braden Mars <bradenmars@bradenmars.me>
2023-05-16 22:44:14 -0500

    feat(projen.component.tool-versions): implement `ToolVersions` projen component.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/projen/component/tool-versions/src/tool-versions.ts
@@ -0,0 +1,87 @@
+import { Component, type Project, TextFile } from 'projen'
+
+export interface ToolVersionsOptions {
+	/**
+	 * Record of asdf/rtx tools.
+	 */
+	readonly tools: Record<string, Array<string>>
+}
+
+/**
+ * ASDF/RTX tool versions.
+ */
+export class ToolVersions extends Component {
+	public static of(project: Project): ToolVersions | undefined {
+		const isToolVersions = (c: Component): c is ToolVersions =>
+			c instanceof ToolVersions
+		return project.components.find(isToolVersions)
+	}
+
+	#tools: Map<string, Array<string>>
+	public readonly file: TextFile
+
+	constructor(
+		project: Project,
+		public readonly options: ToolVersionsOptions = { tools: {} }
+	) {
+		super(project)
+		const { tools } = options
+		this.#tools = new Map(Object.entries(tools))
+		this.file = new TextFile(project, '.tool-versions', {
+			readonly: true,
+			marker: true,
+		})
+	}
+
+	/**
+	 * Retrieve versions for given tool name.
+	 * @param toolName Name of tool.
+	 */
+	versionsOf(toolName: string): string[] {
+		return this.#tools.get(toolName) ?? []
+	}
+
+	/**
+	 * Remove all versions for tool.
+	 * @param toolName Name of tool.
+	 */
+	removeTool(toolName: string): this {
+		this.#tools.delete(toolName)
+		return this
+	}
+
+	/**
+	 * Overwrite versions for given tool.
+	 * @param toolName Name of tool.
+	 * @param versions Accepted versions.
+	 */
+	setVersions(toolName: string, versions: string[]): this {
+		this.#tools.set(toolName, versions)
+		return this
+	}
+
+	/**
+	 * Concat additional versions for a given tool.
+	 * @param toolName Name of tool.
+	 * @param versions Additional versions.
+	 */
+	concatVersions(toolName: string, versions: string[]): this {
+		const current = this.#tools.get(toolName) ?? []
+		return this.setVersions(
+			toolName,
+			Array.from(new Set([...current, ...versions]))
+		)
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	preSynthesize() {
+		super.preSynthesize()
+		const entries = Array.from(this.#tools.entries())
+		for (const [toolName, versions] of entries) {
+			const line = `${toolName} ${versions.join(' ')}`
+			this.file.addLine(line)
+		}
+	}
+}

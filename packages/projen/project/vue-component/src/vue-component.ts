00fb5636ae0c3fa183c253135a7d112d524e4527
Braden Mars <bradenmars@bradenmars.me>
2023-05-23 03:16:06 -0500

    feat(projen.project.vue-component): utilize vite to bundle vue component projects.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/projen/project/vue-component/src/vue-component.ts
+++ packages/projen/project/vue-component/src/vue-component.ts
@@ -1,3 +1,4 @@
+import { Vite } from '@arroyodev-llc/projen.component.vite'
 import { Vue } from '@arroyodev-llc/projen.component.vue'
 import {
 	TypescriptProject,
@@ -18,6 +19,50 @@ export class VueComponentProject extends TypescriptProject {
 	protected applyPackage(): this {
 		super.applyPackage()
 		this.package.addField('sideEffects', true)
+		this.package.addField('module', 'dist/index.js')
+		this.package.addField('main', 'dist/index.cjs')
+		this.package.addField('exports', {
+			'.': {
+				import: 'dist/index.js',
+				types: 'dist/index.dts',
+				require: 'dist/index.cjs',
+			},
+		})
+		return this
+	}
+
+	protected applyBundler(): this {
+		new Vite(this)
+			.addPlugin({
+				name: 'tsconfigPaths',
+				moduleImport: {
+					moduleSpecifier: 'vite-tsconfig-paths',
+					defaultImport: 'tsconfigPaths',
+				},
+			})
+			.addPlugin({
+				name: 'dts',
+				options: {
+					entryRoot: 'src',
+				},
+				moduleImport: {
+					moduleSpecifier: 'vite-plugin-dts',
+					defaultImport: 'dts',
+				},
+			})
+			.addBuildConfig({
+				build: {
+					lib: {
+						entry: 'src/index.ts',
+						fileName: 'index',
+						name: this.projectName.packageName,
+						formats: ['es', 'cjs'],
+					},
+					rollupOptions: {
+						external: ['vue', '@vue/runtime-dom'],
+					},
+				},
+			})
 		return this
 	}
 }

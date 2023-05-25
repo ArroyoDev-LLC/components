6052418779036aedae2ed4ac27a3a24a8d59f4e3
Braden Mars <bradenmars@bradenmars.me>
2023-05-25 02:53:46 -0500

    feat(projen.component.ts-source): implement `TypeScriptSourceConfig` component.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/projen/component/typescript-source-file/src/typescript-source-config.ts
@@ -0,0 +1,117 @@
+import { addPropertyAssignmentsFromObject } from '@arroyodev-llc/utils.projen'
+import { DependencyType } from 'projen'
+import { type TypeScriptProject } from 'projen/lib/typescript'
+import {
+	type ImportDeclarationStructure,
+	type ObjectLiteralExpression,
+	type OptionalKind,
+	type SourceFile,
+	SyntaxKind,
+	type WriterFunction,
+} from 'ts-morph'
+import {
+	TypeScriptSourceFile,
+	type TypeScriptSourceFileOptions,
+	type TypeScriptSourceFileTransform,
+} from './typescript-source-file'
+
+export interface TypeScriptSourceConfigOptions<ConfigT = Record<string, any>>
+	extends TypeScriptSourceFileOptions {
+	config?: ConfigT
+	pluginsProperty?: string
+}
+
+export interface TypeScriptSourceConfigPlugin<OptionsT = Record<string, any>> {
+	/**
+	 * Plugin name.
+	 */
+	name: string
+	/**
+	 * Plugin import.
+	 */
+	moduleImport: OptionalKind<ImportDeclarationStructure>
+	/**
+	 * Plugin definition specification.
+	 * Defaults to `<name>(<options>)`.
+	 */
+	spec?: WriterFunction | readonly (string | WriterFunction)[]
+	/**
+	 * Plugin options.
+	 */
+	options?: OptionsT
+}
+
+export class TypeScriptSourceConfig<
+	T extends object
+> extends TypeScriptSourceFile {
+	constructor(
+		project: TypeScriptProject,
+		filePath: string,
+		public readonly options: TypeScriptSourceConfigOptions<T>
+	) {
+		const { config, pluginsProperty, ...rest } = options
+		super(project, filePath, rest)
+		if (config) {
+			this.addConfig(config)
+		}
+	}
+
+	/**
+	 * Merge configuration.
+	 * @param config
+	 */
+	addConfig(config: T): this {
+		this.addConfigTransform((configExpr) => {
+			addPropertyAssignmentsFromObject(configExpr, config)
+		})
+		return this
+	}
+
+	/**
+	 * Add plugin to config.
+	 * @param plugin Plugin spec to add.
+	 */
+	addPlugin(plugin: TypeScriptSourceConfigPlugin): this {
+		const { spec, name, moduleImport, options } = plugin
+		const pluginSpec =
+			spec ??
+			((writer) =>
+				writer.write(`${name}(${options ? JSON.stringify(options) : ''})`))
+
+		this.project.deps.addDependency(
+			moduleImport.moduleSpecifier,
+			DependencyType.RUNTIME
+		)
+		this.addImport(moduleImport)
+		this.addConfigTransform((configExpr) => {
+			const existsPlugins = this.getOrCreatePropertyAssignmentInitializer(
+				configExpr,
+				this.options.pluginsProperty ?? 'plugins',
+				SyntaxKind.ArrayLiteralExpression
+			)
+			existsPlugins.addElements(pluginSpec)
+		})
+		return this
+	}
+
+	/**
+	 * Transform config.
+	 * @param transform Transformation to apply.
+	 */
+	addConfigTransform(
+		transform: (
+			configObjectLiteral: ObjectLiteralExpression,
+			sourceFile: SourceFile
+		) => void
+	) {
+		const transformer: TypeScriptSourceFileTransform = (src: SourceFile) => {
+			const configExport = this.getDefaultExport(
+				src,
+				SyntaxKind.ObjectLiteralExpression
+			)
+			transform(configExport, src)
+		}
+		this.addTransformer(transformer)
+		return this
+	}
+}

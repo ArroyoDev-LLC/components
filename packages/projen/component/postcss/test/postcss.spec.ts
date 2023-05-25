f558553837d3491e632f46b4c4a89d40a060a288
Braden Mars <bradenmars@bradenmars.me>
2023-05-25 03:31:18 -0500

    test(projen.component.postcss): add unit tests
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/projen/component/postcss/test/postcss.spec.ts
@@ -0,0 +1,49 @@
+import os from 'node:os'
+import path from 'node:path'
+import { typescript } from 'projen'
+import { Testing } from 'projen/lib/testing'
+import { beforeEach, expect, test } from 'vitest'
+import { PostCSS } from '../src'
+
+interface TestContext {
+	project: typescript.TypeScriptProject
+}
+
+beforeEach<TestContext>((ctx) => {
+	ctx.project = new typescript.TypeScriptProject({
+		name: ctx.meta.id,
+		outdir: path.join(os.tmpdir(), ctx.meta.id),
+		defaultReleaseBranch: 'main',
+	})
+})
+
+test<TestContext>('renders as expected', async (ctx) => {
+	new PostCSS(ctx.project)
+	const synth = Testing.synth(ctx.project)
+	expect(synth['postcss.config.ts']).toBeDefined()
+	expect(synth['postcss.config.ts']).toMatchSnapshot()
+})
+
+test<TestContext>('renders with plugins as expected', async (ctx) => {
+	const postcss = new PostCSS(ctx.project)
+	postcss
+		.addPlugin({
+			name: 'tailwindcss',
+			moduleImport: {
+				moduleSpecifier: 'tailwindcss',
+				defaultImport: 'tailwindcss',
+			},
+		})
+		.addPlugin({
+			name: 'postcssPxToRem',
+			moduleImport: {
+				moduleSpecifier: 'postcss-pxtorem',
+				defaultImport: 'postcssPxToRem',
+			},
+			options: {
+				propList: ['font'],
+			},
+		})
+	const synth = Testing.synth(ctx.project)
+	expect(synth['postcss.config.ts']).toMatchSnapshot()
+})

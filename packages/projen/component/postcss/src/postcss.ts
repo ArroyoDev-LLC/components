4f507e3e0c512765afa3bb78fef6f5c6bfb47d66
Braden Mars <bradenmars@bradenmars.me>
2023-05-25 03:20:44 -0500

    feat(projen.component.postcss): implement `PostCSS` component.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/projen/component/postcss/src/postcss.ts
@@ -0,0 +1,53 @@
+import {
+	TypeScriptSourceConfig,
+	type TypeScriptSourceConfigPlugin,
+} from '@arroyodev-llc/projen.component.typescript-source-file'
+import { findComponent } from '@arroyodev-llc/utils.projen'
+import type { Config } from 'postcss-load-config'
+import { Component, type Project, type typescript } from 'projen'
+
+export interface PostCSSOptions {
+	readonly filePath?: string
+	readonly config?: Config
+}
+
+export class PostCSS extends Component {
+	public static of(project: Project): PostCSS | undefined {
+		return findComponent(project, PostCSS)
+	}
+
+	readonly file: TypeScriptSourceConfig<Config>
+
+	constructor(project: typescript.TypeScriptProject, options?: PostCSSOptions) {
+		super(project)
+
+		this.file = new TypeScriptSourceConfig(
+			project,
+			options?.filePath ?? 'postcss.config.ts',
+			{
+				marker: true,
+				pluginsProperty: 'plugins',
+				recreate: true,
+				source: `export default {}`,
+			}
+		)
+	}
+
+	addPlugin(plugin: TypeScriptSourceConfigPlugin): this {
+		const spec = {
+			...(plugin.options
+				? {
+						spec: (writer) => writer.write(plugin.name),
+				  }
+				: {}),
+			...plugin,
+		} satisfies TypeScriptSourceConfigPlugin
+		this.file.addPlugin(spec)
+		return this
+	}
+
+	addConfig(config: Config) {
+		this.file.addConfig(config)
+		return this
+	}
+}

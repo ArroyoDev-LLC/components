f9208467be80146e3dd1bd50726ea647910d81cb
Braden Mars <bradenmars@bradenmars.me>
2023-07-18 01:38:31 -0500

    feat(utils.projen): implement `ProjectBuilder` for reusable build logic
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/utils/projen/src/builder.ts
+++ packages/utils/projen/src/builder.ts
@@ -32,3 +32,52 @@ export abstract class BuilderStep {
 	): TypedPropertyDescriptorMap<this['outputType']>
 }
 
+export class ProjectBuilder<
+	T extends GenericProjectConstructor = GenericProjectConstructor,
+	Options extends ProjectConstructorOptions<T> = ProjectConstructorOptions<T>
+> {
+	declare __outputType: T
+	declare __optionsType: Options
+
+	constructor(
+		readonly projectConstructor: T,
+		readonly defaultOptions: Array<Partial<Options>> = [],
+		readonly steps: Array<BuilderStep> = []
+	) {}
+
+	add<StepT extends BuilderStep>(step: StepT) {
+		const builder = new ProjectBuilder(
+			this.projectConstructor as unknown as GConstructor<
+				InstanceType<T> & (typeof step)['outputType'],
+				[options: Options & (typeof step)['outputOptionsType']]
+			>,
+			this.defaultOptions as Array<
+				Partial<Options & (typeof step)['outputOptionsType']>
+			>,
+			[...this.steps, step]
+		)
+		return builder
+	}
+
+	buildOptions(options: Options) {
+		const mappedOptions = this.steps.reduce(
+			(acc, step) =>
+				({ ...acc, ...step.applyOptions(acc as Options) } as Options),
+			Object.assign({}, options) as object
+		) as Options
+
+		return withDefaults(...this.defaultOptions)(mappedOptions)
+	}
+
+	build(options: Options): InstanceType<T> {
+		const finalOptions = this.buildOptions(options)
+		const project = new this.projectConstructor(finalOptions)
+
+		const properties = this.steps.reduce(
+			(acc, step) => ({ ...acc, ...step.applyProject(project) }),
+			{} as PropertyDescriptorMap
+		)
+		return Object.defineProperties(project, properties) as InstanceType<T>
+	}
+}
+

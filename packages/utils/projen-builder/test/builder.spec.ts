85b8eb6ae748be561804272c1b4d24aaad7f5546
Braden Mars <bradenmars@bradenmars.me>
2023-07-18 03:02:31 -0500

    test(utils.projen-builder): add unit tests
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/utils/projen-builder/test/builder.spec.ts
@@ -0,0 +1,115 @@
+import {
+	javascript,
+	type Project,
+	type ProjectOptions,
+	typescript,
+} from 'projen'
+import { describe, expect, test } from 'vitest'
+import { builders, type BuildStep, ProjectBuilder } from '../src'
+import { type TypedPropertyDescriptorMap } from '../src/types'
+
+describe.concurrent('ProjectBuilder', () => {
+	class CustomBuilder implements BuildStep {
+		declare outputOptionsType: {
+			readonly newOption?: boolean
+			readonly defaultReleaseBranch?: string
+		}
+		declare outputType: {
+			readonly customProperty: string
+		}
+
+		applyOptions(
+			options: ProjectOptions & this['outputOptionsType']
+		): ProjectOptions & this['outputOptionsType'] {
+			return {
+				...options,
+				defaultReleaseBranch: 'override',
+				newOption: options.newOption ?? true,
+			}
+		}
+
+		applyProject(
+			project: Project
+		): TypedPropertyDescriptorMap<this['outputType']> {
+			return {
+				customProperty: {
+					writable: false,
+					value: 'customValue',
+				},
+			} as TypedPropertyDescriptorMap<this['outputType']>
+		}
+	}
+
+	test('handles default values', () => {
+		const builder = new ProjectBuilder(typescript.TypeScriptProject, [
+			{
+				defaultReleaseBranch: 'main',
+				packageManager: javascript.NodePackageManager.PNPM,
+			},
+			{ github: true, githubOptions: { pullRequestLint: true, mergify: true } },
+			{ githubOptions: { pullRequestLintOptions: { semanticTitle: true } } },
+		])
+		const options = builder.buildOptions({
+			defaultReleaseBranch: 'other',
+			name: 'test.project',
+		})
+		expect(options).toMatchInlineSnapshot()
+	})
+
+	test('applies step options', () => {
+		const options = new ProjectBuilder(typescript.TypeScriptProject)
+			.add(new CustomBuilder())
+			.buildOptions({
+				name: 'test.project',
+				defaultReleaseBranch: 'willOverride',
+			})
+		expect(options).toMatchInlineSnapshot(`
+			{
+			  "defaultReleaseBranch": "override",
+			  "name": "test.project",
+			  "newOption": true,
+			}
+		`)
+	})
+
+	test('applies step project', () => {
+		const project = new ProjectBuilder(typescript.TypeScriptProject)
+			.add(new CustomBuilder())
+			.build({
+				name: 'test.project',
+				defaultReleaseBranch: 'willOverride',
+			})
+		expect(project.customProperty).toBe('customValue')
+	})
+})
+
+describe.concurrent('builders', () => {
+	const base = new ProjectBuilder(typescript.TypeScriptProject)
+
+	test('NameSchemeBuilder', () => {
+		const project = base
+			.add(new builders.NameSchemeBuilder({ scope: '@test' }))
+			.build({
+				name: 'test.project',
+				defaultReleaseBranch: 'main',
+			})
+		expect(project.package.packageName).toBe('@test/test.project')
+	})
+
+	test('OptionsPropertyBuilder', () => {
+		const project = base
+			.add(
+				new builders.OptionsPropertyBuilder<(typeof base)['__optionsType']>()
+			)
+			.build({
+				name: 'test.project',
+				defaultReleaseBranch: 'main',
+			})
+		expect(project.options).toMatchInlineSnapshot(`
+			{
+			  "defaultReleaseBranch": "main",
+			  "name": "test.project",
+			}
+		`)
+	})
+})

8da216ab1b35621a6bc9f36e8fe1bec604918c7b
Braden Mars <bradenmars@bradenmars.me>
2023-07-18 20:48:02 -0500

    feat(projen.project.vue-component): implement `VueComponentBaseBuilder`
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/projen/project/vue-component/src/vue-component.ts
+++ packages/projen/project/vue-component/src/vue-component.ts
@@ -2,9 +2,95 @@ import { Vite } from '@arroyodev-llc/projen.component.vite'
 import { Vue } from '@arroyodev-llc/projen.component.vue'
 import {
 	TypescriptProject,
+	TypescriptBaseBuilder,
 	type TypeScriptProjectOptions,
 } from '@arroyodev-llc/projen.project.typescript'
+import {
+	BaseBuildStep,
+	builders,
+	type TypedPropertyDescriptorMap,
+} from '@arroyodev-llc/utils.projen-builder'
+import { type typescript } from 'projen'
 
+export class VueComponentViteBuilder extends BaseBuildStep<
+	{},
+	{ readonly vite: Vite }
+> {
+	applyProject(
+		project: typescript.TypeScriptProject
+	): TypedPropertyDescriptorMap<this['outputType']> {
+		const vite = new Vite(project)
+			.addPlugin({
+				name: 'tsconfigPaths',
+				moduleImport: {
+					moduleSpecifier: 'vite-tsconfig-paths',
+					defaultImport: 'tsconfigPaths',
+				},
+			})
+			.addPlugin({
+				name: 'dts',
+				options: {
+					entryRoot: 'src',
+				},
+				moduleImport: {
+					moduleSpecifier: 'vite-plugin-dts',
+					defaultImport: 'dts',
+				},
+			})
+			.addBuildConfig({
+				build: {
+					lib: {
+						entry: 'src/index.ts',
+						fileName: 'index',
+						name: project.package.packageName,
+						formats: ['es', 'cjs'],
+					},
+					rollupOptions: {
+						external: ['vue', '@vue/runtime-dom'],
+					},
+				},
+			})
+		return {
+			vite: { writable: false, value: vite },
+		} as TypedPropertyDescriptorMap<this['outputType']>
+	}
+}
+
+export class VueComponentManifestBuilder extends BaseBuildStep<
+	{},
+	{
+		readonly vue: Vue
+	}
+> {
+	applyProject(
+		project: typescript.TypeScriptProject
+	): TypedPropertyDescriptorMap<this['outputType']> {
+		project.package.addField('sideEffects', true)
+		project.package.addField('module', 'dist/index.js')
+		project.package.addField('main', 'dist/index.cjs')
+		project.package.addField('exports', {
+			'.': {
+				import: 'dist/index.js',
+				types: 'dist/index.dts',
+				require: 'dist/index.cjs',
+			},
+		})
+		return {
+			vue: { writable: false, value: new Vue(project) },
+		} as TypedPropertyDescriptorMap<this['outputType']>
+	}
+}
+
+export const VueComponentBaseBuilder = TypescriptBaseBuilder.add(
+	new builders.DefaultOptionsBuilder({ release: true, unbuild: false }),
+	{ prepend: true }
+)
+	.add(new VueComponentViteBuilder())
+	.add(new VueComponentManifestBuilder())
+
+/**
+ * @deprecated Use `VueComponentBaseBuilder` instead
+ */
 export class VueComponentProject extends TypescriptProject {
 	public readonly vue: Vue
 

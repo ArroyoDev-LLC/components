4137d234011f4de614ea90c1e62aa8a47be8735d
Braden Mars <bradenmars@bradenmars.me>
2023-07-18 21:03:01 -0500

    feat(projen.project.vue-component): remove `VueComponentProject`
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/projen/project/vue-component/src/vue-component.ts
+++ packages/projen/project/vue-component/src/vue-component.ts
@@ -1,10 +1,6 @@
 import { Vite } from '@arroyodev-llc/projen.component.vite'
 import { Vue } from '@arroyodev-llc/projen.component.vue'
-import {
-	TypescriptProject,
-	TypescriptBaseBuilder,
-	type TypeScriptProjectOptions,
-} from '@arroyodev-llc/projen.project.typescript'
+import { TypescriptBaseBuilder } from '@arroyodev-llc/projen.project.typescript'
 import {
 	BaseBuildStep,
 	builders,
@@ -87,68 +83,3 @@ export const VueComponentBaseBuilder = TypescriptBaseBuilder.add(
 )
 	.add(new VueComponentViteBuilder())
 	.add(new VueComponentManifestBuilder())
-
-/**
- * @deprecated Use `VueComponentBaseBuilder` instead
- */
-export class VueComponentProject extends TypescriptProject {
-	public readonly vue: Vue
-
-	constructor(options: TypeScriptProjectOptions) {
-		super({
-			release: true,
-			...options,
-		})
-		this.vue = new Vue(this)
-	}
-
-	protected applyPackage(): this {
-		super.applyPackage()
-		this.package.addField('sideEffects', true)
-		this.package.addField('module', 'dist/index.js')
-		this.package.addField('main', 'dist/index.cjs')
-		this.package.addField('exports', {
-			'.': {
-				import: 'dist/index.js',
-				types: 'dist/index.dts',
-				require: 'dist/index.cjs',
-			},
-		})
-		return this
-	}
-
-	protected applyBundler(): this {
-		new Vite(this)
-			.addPlugin({
-				name: 'tsconfigPaths',
-				moduleImport: {
-					moduleSpecifier: 'vite-tsconfig-paths',
-					defaultImport: 'tsconfigPaths',
-				},
-			})
-			.addPlugin({
-				name: 'dts',
-				options: {
-					entryRoot: 'src',
-				},
-				moduleImport: {
-					moduleSpecifier: 'vite-plugin-dts',
-					defaultImport: 'dts',
-				},
-			})
-			.addBuildConfig({
-				build: {
-					lib: {
-						entry: 'src/index.ts',
-						fileName: 'index',
-						name: this.projectName.packageName,
-						formats: ['es', 'cjs'],
-					},
-					rollupOptions: {
-						external: ['vue', '@vue/runtime-dom'],
-					},
-				},
-			})
-		return this
-	}
-}

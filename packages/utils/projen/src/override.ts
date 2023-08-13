3b57a3e4688299e62deeffb825bf79a4ef293aaa
Braden Mars <bradenmars@bradenmars.me>
2023-08-13 00:08:22 -0500

    feat(utils.projen): auto-flatten override objects in `applyOverrides`, `extendArrays` boolean option
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/utils/projen/src/override.ts
+++ packages/utils/projen/src/override.ts
@@ -1,21 +1,47 @@
+import flatten from 'flat'
+
 /**
  * Generic interface an object that supports overrides.
  */
 interface SupportsOverrides {
 	addOverride(key: string, value: any): void
+	addToArray(key: string, ...values: any[]): void
+}
+
+interface ApplyOverridesOptions {
+	/**
+	 * If true, extends existing arrays.
+	 * Otherwise, replaces them.
+	 * @default false
+	 */
+	extendArrays: boolean
 }
 
 /**
  * Apply overrides from an object.
- * @param item - item to apply too.
- * @param overrides - record of overrides.
+ * @param item item to apply too.
+ * @param overrides record of overrides.
+ * @param options behavior options.
  */
 export const applyOverrides = <T extends SupportsOverrides>(
 	item: T,
-	overrides: Record<string, any>
+	overrides: Record<string, any>,
+	options?: ApplyOverridesOptions
 ): T => {
-	Object.entries(overrides).forEach(([key, value]) =>
-		item.addOverride(key, value)
+	const extendArrays = options?.extendArrays ?? false
+	const flatItem: Record<string, any | Array<any>> = flatten.flatten(
+		overrides,
+		{
+			delimiter: '.',
+			safe: true,
+		}
 	)
+	Object.entries(flatItem).forEach(([key, value]) => {
+		if (Array.isArray(value) && extendArrays) {
+			item.addToArray(key, ...(value as unknown[]))
+		} else {
+			item.addOverride(key, value)
+		}
+	})
 	return item
 }

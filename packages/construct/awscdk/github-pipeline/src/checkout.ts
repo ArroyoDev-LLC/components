b122af4564afa44d394cde6bc4a160eeb62af4ea
Braden Mars <bradenmars@bradenmars.me>
2023-08-21 16:48:18 -0500

    feat(construct.awscdk.github-pipeline): `CheckoutStep` for checkout action
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/construct/awscdk/github-pipeline/src/checkout.ts
@@ -0,0 +1,44 @@
+import * as ghpipelines from 'cdk-pipelines-github'
+
+export interface CheckoutProps {
+	/**
+	 * The version of the action to use.
+	 * @default v3
+	 */
+	actionVersion?: string
+	/**
+	 * The name of the repository to checkout.
+	 */
+	repository?: string
+	/**
+	 * The branch, tag or SHA to checkout.
+	 */
+	ref?: string
+	/**
+	 * Path to write repository contents to.
+	 */
+	path?: string
+}
+
+/**
+ * Checkout a repository.
+ */
+export class CheckoutStep extends ghpipelines.GitHubActionStep {
+	constructor(id: string, props: CheckoutProps) {
+		const { actionVersion = 'v3', ...withProps } = props
+		const name = withProps?.repository
+			? 'Checkout'
+			: `Checkout ${withProps.repository!}`
+		const actionWith = Object.keys(withProps).length ? { with: withProps } : {}
+		const actionUses = `actions/checkout@${actionVersion}`
+		super(id, {
+			jobSteps: [
+				{
+					name,
+					uses: actionUses,
+					...actionWith,
+				},
+			],
+		})
+	}
+}
--- packages/construct/awscdk/github-pipeline/src/index.ts
+++ packages/construct/awscdk/github-pipeline/src/index.ts
@@ -1,2 +1,3 @@
 export * from './github-pipeline'
 export * from './s3'
+export * from './checkout'

598e71fdcef744a084ae1260287c9ea6744c9ddf
Braden Mars <bradenmars@bradenmars.me>
2023-08-22 18:59:08 -0500

    test(construct.awscdk.github-pipeline): unit tests for multi-stage, code pipeline, related
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/construct/awscdk/github-pipeline/test/workflow.spec.ts
+++ packages/construct/awscdk/github-pipeline/test/workflow.spec.ts
@@ -1,43 +1,164 @@
 import fs from 'node:fs/promises'
 import os from 'node:os'
 import path from 'node:path'
-import { App, Stack, Stage } from 'aws-cdk-lib'
+import {
+	App,
+	Stack,
+	type StackProps,
+	Stage,
+	type StageProps,
+} from 'aws-cdk-lib'
 import * as s3 from 'aws-cdk-lib/aws-s3'
-import { describe, test, expect } from 'vitest'
-import { GithubCodePipeline } from '../src'
+import { type Construct } from 'constructs'
+import { describe, test, expect, beforeEach, afterAll } from 'vitest'
+import { GithubCodePipeline, type GithubWorkflowPipeline } from '../src'
+
+interface TestContext {
+	rootDir: string
+	app: App
+	stack: typeof Stack
+	stage: { new (...args: ConstructorParameters<typeof Stage>): Stage }
+}
+
+const TEST_DIR = path.join(os.tmpdir(), 'github-pipeline-test')
+
+afterAll(async () => fs.rmdir(TEST_DIR, { maxRetries: 0 }).catch(() => {}))
 
 describe('GithubCodePipeline', () => {
-	test('synthesizes minimal expected pipeline', async () => {
-		const rootDir = await fs.mkdtemp(
-			path.join(os.tmpdir(), 'test-stack-pipeline'),
+	beforeEach<TestContext>(async (ctx) => {
+		const tmpDir = path.join(TEST_DIR, ctx.task.name.replace(/\s/g, '-'))
+		await fs.rmdir(tmpDir).catch(() => {})
+		const rootDir = await fs.mkdir(tmpDir, { recursive: true })
+		ctx.rootDir = tmpDir
+		ctx.app = new App({ outdir: rootDir })
+		ctx.stack = class extends Stack {
+			constructor(scope: Construct, id: string, stackProps?: StackProps) {
+				super(scope, id, stackProps)
+				new s3.Bucket(this, id + '-test-sample-bucket')
+			}
+		}
+		ctx.stage = class extends Stage {
+			constructor(scope: Construct, id: string, stageProps?: StageProps) {
+				super(scope, id, stageProps)
+				new ctx.stack(this, id + '-stack')
+			}
+		}
+	})
+
+	const buildMultiStage = (
+		ctx: TestContext,
+	): [pipe: GithubWorkflowPipeline, builder: GithubCodePipeline] => {
+		const builder = GithubCodePipeline.create({
+			rootDir: ctx.rootDir,
+			assetsS3Prefix: 'multi-stage',
+			assetsS3Bucket: 'test-stack',
+			workflowName: 'Deploy Many Stages',
+		})
+			.addConfigsEnv()
+			.addNxEnv()
+			.synthTarget({
+				workingDirectory: 'packages/test-many',
+				packageName: 'test.many',
+			})
+			.installHelm('3.6.3')
+			.installAwsCli('2')
+			.installSops('3.7.3')
+			.installPnpm('18', '8')
+			.publishPostStep({
+				name: 'My Custom post publish step',
+				run: 'echo "hello world"',
+			})
+
+		const pipe = builder
+			.build(ctx.app)
+			.concurrency({ cancelInProgress: false, group: 'deploy-multi-stack' })
+			.onWorkflowDispatch()
+			.onWorkflowCall({
+				runner: {
+					type: 'string',
+					default: 'self-hosted',
+					description: 'custom runner',
+					required: false,
+				},
+			})
+
+		pipe.addStageWithGitHubOptions(
+			new ctx.stage(ctx.app, 'alpha', {
+				env: { account: '123', region: 'us-east-1' },
+			}),
 		)
 
-		const app = new App({ outdir: rootDir })
+		pipe.addStageWithGitHubOptions(
+			new ctx.stage(ctx.app, 'beta', {
+				env: { account: '456', region: 'us-east-1' },
+			}),
+		)
 
-		const stage = new Stage(app, 'test', {
-			env: { account: '1234567890', region: 'us-east-1' },
-		})
-		const stack = new Stack(stage, 'test-stack', {
-			env: { account: '1234567890', region: 'us-east-1' },
-		})
-		new s3.Bucket(stack, 'test-bucket')
+		pipe.addStageWithGitHubOptions(
+			new ctx.stage(ctx.app, 'delta', {
+				env: { account: '789', region: 'us-east-1' },
+			}),
+		)
+		return [pipe, builder]
+	}
+
+	test<TestContext>('synthesizes minimal expected pipeline', async (ctx) => {
+		const env = { account: '123', region: 'us-east-1' }
 
 		const pipe = GithubCodePipeline.create({
 			assetsS3Bucket: 'test-bucket',
 			workflowName: 'Deploy Test Stack',
 			assetsS3Prefix: 'test-stack',
-			rootDir,
+			rootDir: ctx.rootDir,
 		})
 			.synthTarget({
 				workingDirectory: 'packages/test-stack',
 				packageName: 'test-stack',
 			})
-			.build(app)
+			.build(ctx.app)
 
-		pipe.addStageWithGitHubOptions(stage)
-		app.synth()
+		pipe.addStageWithGitHubOptions(new ctx.stage(ctx.app, 'test', { env }))
+		ctx.app.synth()
 
 		const contents = (await fs.readFile(pipe.workflowPath)).toString()
 		expect(contents).toMatchSnapshot()
+	})
+
+	test<TestContext>('synthesizes as expected with many stages', async (ctx) => {
+		const [pipe, _] = buildMultiStage(ctx)
+		ctx.app.synth()
+		const contents = (await fs.readFile(pipe.workflowPath)).toString()
+		expect(contents).toMatchSnapshot()
+	})
+
+	test<TestContext>('masks all stage account ids', async (ctx) => {
+		const [pipe, _] = buildMultiStage(ctx)
+		ctx.app.synth()
+		const contents = (await fs.readFile(pipe.workflowPath)).toString()
+		expect(contents).not.to.include('123')
+		expect(contents).not.to.include('456')
+		expect(contents).not.to.include('789')
+		expect(contents).to.include('AWS_ACCOUNT_ID_ALPHA')
+		expect(contents).to.include('AWS_ACCOUNT_ID_BETA')
+		expect(contents).to.include('AWS_ACCOUNT_ID_DELTA')
+	})
+
+	test<TestContext>('builder has expected props', (ctx) => {
+		const [_, builder] = buildMultiStage(ctx)
+		expect(builder.props).toMatchSnapshot()
+		expect(builder.synthProps).toMatchSnapshot()
+		expect(builder).toMatchSnapshot()
+	})
+
+	test<TestContext>('synthesize prebuild steps are in expected order', (ctx) => {
+		const [_, builder] = buildMultiStage(ctx)
+		const awsIdx = builder.props.preBuildSteps!.findIndex(
+			(step) => step.name === 'Install AWS CLI',
+		)
+		const pullCtxIdx = builder.props.preBuildSteps!.findIndex(
+			(step) => step.name === 'Pull cdk.context.json',
+		)
+		console.log(builder.props.preBuildSteps)
+		expect(awsIdx).toBeLessThan(pullCtxIdx)
 	})
 })
